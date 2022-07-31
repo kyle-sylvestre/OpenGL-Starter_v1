dfc74a18f698cf91ea7b2e57bfff6653ddd2f9d9
kyle-sylvestre <kyles3133@gmail.com>
2022-07-30 21:31:45 -0400

    create src, intermediate, and output folders

--- make.bat
+++ make.bat
@@ -1,22 +1,29 @@
-call dev.bat
-set EXE=3DSage_Starter.exe
+@echo off
+subst w: .
+rem subst w: /D
+set EXE=w:\output\app.exe
+set SRC=w:\src\3DSage_Starter.cpp
+set LIBS=winmm.lib user32.lib gdi32.lib opengl32.lib glu32.lib w:\glut\Win32_2010\Debug\glutstatic.lib 
 
 if "%1"=="clean" (
-    del *.pdb
-    del *.obj
-    del *.exe
+    del .\intermediate\* /Q
+    del .\output\* /Q
     rem msbuild glut\glut_2010.vcxproj /t:clean /p:Configuration=Debug /p:Platform=X86
 ) 
 
 if "%1"=="" (
-    set LIBS=winmm.lib user32.lib gdi32.lib opengl32.lib glu32.lib glut\Win32_2010\Debug\glutstatic.lib 
-    rem msbuild glut\glut_2010.vcxproj /p:DefineConstants=GLUT_BUILDING_LIB /p:Configuration=Debug /p:Platform=X86 /m
-    cl -VERBOSE -MP -Zi 3DSage_Starter.cpp -Iglut\include\GL -link %LIBS%
+    rem TODO: how to /DGLUT_BUILDING_LIB in msbuild line
+    rem msbuild w:\glut\glut_2010.vcxproj /p:Configuration=Debug /p:Platform=X86 /m
+    call dev.bat
+    pushd intermediate
+    cl -MP -Zi %SRC% -Iw:\glut\include\GL -Fe"%EXE%" -link %LIBS%
+    popd
 )
 
 if "%1"=="debug" (
     rem for /F "tokens=1,2" %%i in ('tasklist /FI "IMAGENAME eq %EXE%" /fo table /nh') do set pid=%%j
     rem echo %pid%
+    call dev.bat
     devenv.exe %EXE%
 ) 
 
