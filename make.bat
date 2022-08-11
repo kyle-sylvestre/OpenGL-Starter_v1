3e61686635b968c6f7fa51ae3e12862c038b8d0a
kyle-sylvestre <kyles3133@gmail.com>
2022-08-10 21:19:49 -0400

    upgrade to VS2022, fix shortcircuit make and run

--- make.bat
+++ make.bat
@@ -2,7 +2,7 @@
 
 rem call script for devenv, msbuild, and cl
 if "%INCLUDE%"=="" (
-    call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+    call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
 )
 
 rem push old cwd and set it to the batch file's directory 
@@ -10,6 +10,12 @@ pushd "%~dp0"
 set EXE=output\app.exe
 set SRC=src\main.cpp
 set LIBS=winmm.lib user32.lib gdi32.lib opengl32.lib glu32.lib glut\Win32_2010\Debug\glutstatic.lib 
+
+if "%1"=="bar" (
+    rem build-and-run: build then run if it was successful
+    rem NB: "call" is needed to shortcircuit on fail within make.bat
+    call make.bat && %EXE% 
+)
 
 if "%1"=="clean" (
     rmdir /s /q intermediate
@@ -28,17 +34,14 @@ if "%1"=="" (
         msbuild.exe glut\glut_2010.vcxproj /p:Configuration=Debug /p:Platform=X86 /m
     )
 
-    rem delete exe bc you don't shortcircuit in "make && output.exe"
-    del %EXE% 2>NUL
-
-    cl.exe -W4 -MP -Zi -Od %SRC% -Iglut\include\GL -Fd.\intermediate\ -Fo.\intermediate\ -Fe%EXE% -link %LIBS%
+    cl.exe -fsanitize=address -EHsc -MTd -W4 -MP -Zi -Od -Ob2 %SRC% -I..\KBT\third-party\ -Iglut\include\GL -Fd.\intermediate\ -Fo.\intermediate\ -Fe%EXE% -link %LIBS%
 )
 
 if "%1"=="debug" (
     rem for /F "tokens=1,2" %%i in ('tasklist /FI "IMAGENAME eq %EXE%" /fo table /nh') do set pid=%%j
     rem echo %pid%
     call dev.bat
-    devenv.exe %EXE%
+    devenv.exe /debugexe %EXE%
 ) 
 
 if "%1"=="tags" (
